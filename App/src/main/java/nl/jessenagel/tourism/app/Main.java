243586b0f169488719afdf20c4fed15c310d7e48
jmn <me@jessenagel.nl>
2024-06-10 16:24:31 +0200

    Index

--- App/src/main/java/nl/jessenagel/tourism/app/Main.java
+++ App/src/main/java/nl/jessenagel/tourism/app/Main.java
@@ -6,12 +6,14 @@ public class Main {
 
 
     public static void main(String[] args) {
-        if (args.length != 2) {
-            System.out.println("Usage: java main <seed(integer)> <parameterfile>");
+        if (args.length != 3) {
+            System.out.println("Usage: java main <seed(integer)> <parameterfile> <index>");
             System.out.println("args.length=" + args.length);
             System.exit(2);
         }
         TouristConstants.seed = Integer.parseInt(args[0]);
+        TouristConstants.index = Integer.parseInt(args[2]);
+
         LoadInputs.readParameterFile(args[1]);
         if ((TouristConstants.inputFolder.equals("inputfiles/amsterdam/") && !TouristConstants.distanceType.equals("file")) || (TouristConstants.inputFolder.equals("inputfiles/solvable/") && !TouristConstants.distanceType.equals("euclidean"))) {
             System.err.println("ARE YOU SURE ABOUT THE DISTANCE TYPE!?");
